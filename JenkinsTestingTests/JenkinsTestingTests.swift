fa7955ffdff12ff0274f6f42b0c2674f692f8c52
embro15 <jesse.steinberg@mac.com>
2025-05-26 16:55:10 -0400

    adding a test testcase

--- JenkinsTestingTests/JenkinsTestingTests.swift
+++ JenkinsTestingTests/JenkinsTestingTests.swift
@@ -6,11 +6,17 @@
 //
 
 import Testing
+import XCTest
 
 struct JenkinsTestingTests {
 
     @Test func example() async throws {
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+    
+    @Test func testExample() {
+        let sum = 2 + 2
+        XCTAssertEqual(sum, 4, "Math still works!")
     }
 
 }
